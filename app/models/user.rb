933f0d7cb36c007f8fc73eccbfdd900f289c25b6
Sajjad Ahmad <jogimar14@gmail.com>
2021-11-30 18:43:36 +0500

    Add more model tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :fullname, :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates :displayname, length: { minimum: 2 }
+  validates :displayname, length: { maximum: 32 }
+  validates :password_digest, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,24 @@ describe User do
   it "is not valid without an email" do
     build(:user, email: nil).should_not be_valid
   end
+  it "is not valid without a password" do
+    build(:user, password_digest: nil).should_not be_valid
+  end
+  it "is valid with a password" do
+    build(:user, password_digest: "123456").should be_valid
+  end
+  it "is not valid with less than 6 characters and more than 10 characters" do
+    build(:user, password_digest: "12345").should_not be_valid
+  end
+  it "is valid with 6 or more characters and less than or 10 characters" do
+    build(:user, password_digest: "123456").should be_valid
+  end
+  it "should have a unique email" do
+    user = User.create!(fullname:"test", email:"a@a.com", password_digest:"123456", displayname:"test")
+    user2 = User.create(fullname:"test", email: "a@a.com", password_digest: "123456", displayname:"test")
+    user2.should_not be_valid
+    user2.errors[:email].should include("has already been taken")
+  end
   it "is not valid with less than 2 characters and more than 2 characters" do
     build(:user, displayname: "a").should_not be_valid
   end
