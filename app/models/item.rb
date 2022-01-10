c63209204c6aa9d12b0129f21c6e166ebc5bc929
Sajjad Ahmad <jogimar14@gmail.com>
2022-01-10 15:48:34 +0500

    An item belongs to a category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :Category
   validates :title, :description, :price, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: {greater_than: 0}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_130236) do
+ActiveRecord::Schema.define(version: 2021_12_01_123356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_130236) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
